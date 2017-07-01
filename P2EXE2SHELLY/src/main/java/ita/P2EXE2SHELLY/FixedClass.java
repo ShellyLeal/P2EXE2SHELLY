b1be1718b0db83931162d63ffcb965b2fe5df315
shell <shell@LAPTOP-258RIN60>
2017-07-01 17:44:19 -0300

    Update da proteção do formato de arquivo.Update de testes, incluindo execução em ordem.

--- P2EXE2SHELLY/src/main/java/ita/P2EXE2SHELLY/FixedClass.java
+++ P2EXE2SHELLY/src/main/java/ita/P2EXE2SHELLY/FixedClass.java
@@ -6,25 +6,41 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FixedClass {
-	public final void fixedMethod(String FILENAME){
+	public final void fixedMethod(String FILENAME) throws Exception{
 		while (true) {
+			
+			Pattern pattern = Pattern.compile("[^A-Za-z0-9._]"); // padroniza o arquivo
+			Matcher matcher = pattern.matcher(FILENAME);
+			
+			if (matcher.find()){
+				throw new Exception();
+			}
+			
 		    final Scanner console = new Scanner(System.in);
 		    System.out.print("Digite a operacao desejada para realizar no arquivo <R para ler um arquivo, "
 		    		+ "W para escrever em um arquivo, "+ "S para sair>: ");
 		    try {
 		    final String opr = console.nextLine();				// protegido com try/catch contra entradas proibidas
-		    if ("R".equals(opr)){
+		    if ("R".equals(opr)){				// é mais seguro utilizar 'equals' com a string do lado esquerdo
 				BufferedReader br = null;				
 				
 				try {
-					String sCurrentLine = null;				
+					String sCurrentLine = null;				// garantir que a variável está limpa
 
 					br = new BufferedReader(new FileReader(FILENAME));
 
 					while ((sCurrentLine = br.readLine()) != null) {
-						System.out.println(sCurrentLine);
+						matcher = pattern.matcher(sCurrentLine);
+						if (!matcher.find())
+							System.out.println(sCurrentLine);
+						else{
+							br.close(); // precisa fechar o buffer mesmo se der erro no meio
+							throw new Exception();
+						}
 					}
 
 				} catch (IOException e) {
@@ -80,7 +96,8 @@ public class FixedClass {
 		    }
 		
 		}
-		catch (java.util.NoSuchElementException a) {
+		catch (java.util.NoSuchElementException a) {  // para motivos de teste. Quando for simulado o usuário, só é possível simular uma entrada. A segunda entrada será nula e finalizará o programa.
+			console.close();
 			System.out.print("Não foi recebida a entrada devida!Programa encerrado.\n");
 			break;
 		}
